4f209b0e051ae86afa8f253953063f59f4461982
Den Chalov <dchalov@gmail.com>
2026-09-02 14:44:06 +0300

    Polish themed booking selectors

--- elements/top-page.php
+++ elements/top-page.php
@@ -9,7 +9,7 @@ site_apply_device_language();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="color-scheme" content="light">
+    <meta name="color-scheme" content="light dark">
     <script>
         (function () {
             var theme;
